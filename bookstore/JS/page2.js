cb4ad2c999f56a54c369aa8f59637d77b28544b5
DJ <dj@code-crew.org>
2025-03-19 13:12:46 -0500

    debugging example in page2.js

---
+++ bookstore/JS/page2.js
@@ -0,0 +1,13 @@
+//debugging non-browser JS
+debugger;
+console.log("hey y'all");
+
+let x = "Bugs Bunny";
+
+x += ": What's up, Doc!?!"
+
+function characterSays(character) {
+    console.log(character);
+}
+
+characterSays(x);
